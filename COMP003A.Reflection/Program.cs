ea9b54536b126d6070a580372cbef50dce0a5516
raymondgonzalez <collegeeduraymond@gmail.com>
2025-02-25 17:41:13 -0800

    Complete

---
+++ COMP003A.Reflection/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace COMP003A.Reflection;
+// Author: Raymond Gonzalez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Final Coding asignmnt/ discussion
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bye, World (COMP003A)!"); 
+        Console.WriteLine("\n\n1. Over the last 8 weeks, what worked well for me was knowing what to expect. I knew that at the beginning of the week, there would be a discussion, practice assignments, and later on an actual assignment followed by a test. There was no, \"Hello, this week we will be doing something else.\" ");
+        Console.WriteLine("\n2. I used an array of external resources to aid my learning, but ultimately, the answers to the questions I had were always in the lecture notes. The only thing that needed to change was ensuring the structure of the code was not subject to hard coding but rather input-based. The methods I used included a quick Google search and YouTube videos of similar examples. ");
+        Console.WriteLine("\n3. I personally think that this class was structured as well as it could be, and whether it was good or bad depended on our individual schedules. For example, I had so much work due on Tuesday night that needed to be completed by Thursday morning, rendering my Wednesday completely used up with no extra time. On top of that, I had to finish a coding project. I understand that students often start an assignment the day it is due, so having coding assignments due Wednesday night forces one to at least start the assignment. Ultimately, the due date changes to be past Wednesday in most cases.");
+        Console.WriteLine("\n4. In the beginning, I believed this class was going to be easy, but I wasn’t really expecting anything specific regarding the material. I didn’t even know it was going to be a computer programming class until Wednesday morning after reading the course description on my portal. To answer the question, I did not learn what I expected because I had no expectations, but I learned a whole lot.");
+        
+        Console.WriteLine("\n5. Every single concept was new to me, except for comments, which resemble HTML code (<></> and //). Concepts like methods and inputs into a variable were some of the new concepts I encountered.");
+        Console.WriteLine("\n6. As mentioned above, the only concept I had encountered before was making notes in my work, as I have done notes in HTML and CSS programs.");
+        Console.WriteLine("\n7. The instructor does not need any tips; he's got it handled.");
+    }
+}
